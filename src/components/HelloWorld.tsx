4faaad1e2deedfc0847da23764aca4561a91fece
Artur Martins <arturmartinspaiva@gmail.com>
2023-10-04 18:38:21 -0300

    localhost and internal server error

--- src/components/HelloWorld.tsx
+++ src/components/HelloWorld.tsx
@@ -1,7 +1,11 @@
 import React from "react";
 import { Button, Block } from "vcc-ui/dist/components";
+import { useCars } from "../hooks/useCars";
 
 export const HelloWorld: React.FC = () => {
+  const { cars } = useCars();
+
+  console.log(cars);
   return (
     <Block extend={{ padding: 20 }}>
       <Button>Click me!</Button>
