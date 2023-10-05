7e8397fd4f8fc67a2bae41cd7c8e2b9a665242b0
Artur Martins <arturmartinspaiva@gmail.com>
2023-10-05 01:06:20 -0300

    add:  spacer class created

--- src/components/CarCard.tsx
+++ src/components/CarCard.tsx
@@ -1,7 +1,7 @@
 import { Spacer, Text, Flex, Link } from "vcc-ui";
 import { Car } from "../types/car.interface";
 import Image from "next/image";
-import styles from "../../public/css/carCard.module.css";
+import styles from "../../public/css/components/carCard.module.css";
 
 interface CardProps {
   car: Car;
